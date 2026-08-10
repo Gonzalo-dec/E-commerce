40b6abfb8cd54062a80750eb516fdf5240ac4fa2
Gonzalo <gonzamello126@gmail.com>
2026-08-10 15:47:00 -0300

    separar puerto de base de datos del puerto de la app

--- Backend/app.js
+++ Backend/app.js
@@ -5,11 +5,14 @@ const productsRouter = require('./Routes/products');
 const ordersRouter = require('./Routes/orders');
 const authRouter = require('./Routes/auth');
 const categoriesRouter = require('./Routes/categories')
+const path = require('path');
 require('dotenv').config(); 
 
 const app = express();
 app.use(cors())
 app.use(express.json());
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/users', usersRouter);
 
@@ -21,6 +24,8 @@ app.use('/auth', authRouter);
 
 app.use('/categories', categoriesRouter);
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 })
--- Backend/db.js
+++ Backend/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = db.createPool({
     host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
-    port: process.env.PORT,
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER,
     database: process.env.DB_NAME
 });
